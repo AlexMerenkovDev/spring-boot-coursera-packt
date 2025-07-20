30388b19aa899f450d457f22f2f76a7e3d58ef34
Alexandre Amade <133642277+AlexMerenkovDev@users.noreply.github.com>
2025-07-20 17:33:48 +0200

    Step 36 - Making Todo an Entity and Population Todo Data into H2

---
+++ webappfrompackt/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo
+(id, username, description, target_date, done) values (10001, 'alex', 'Learn Java EE', CURRENT_DATE, false);
+
+insert into todo
+(id, username, description, target_date, done) values (10002, 'amelia', 'Learn springboot', CURRENT_DATE, false);
+
+insert into todo
+(id, username, description, target_date, done) values (10003, 'rato', 'Learn to cook', CURRENT_DATE, false);
